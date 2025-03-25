fd687bd7327df76ec5f5d1a50f266d5e3e12ccf7
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-03-25 10:27:03 +0700

    - practice SQL

--- mysql/query_1.sql
+++ mysql/query_1.sql
@@ -1,4 +1,4 @@
-/* =========== Detect triangle */
+/* =========== Detect triangle and condition */
 select
     CASE
         WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'   /* must be the first condition */
@@ -7,7 +7,7 @@ select
         ELSE 'Scalene'
     END
 from triangles;
-/* =========== CONCAT string */
+/* =========== CONCAT and Sub string */
 (select
  concat(name, '(', left(Occupation, 1), ')') as name
 from occupations
@@ -19,10 +19,38 @@ from occupations
 group by Occupation
 order by count(Occupation) ASC, Occupation ASC
 );
-/* =========== */
+/* =========== partition */
 uncompleted
 SELECT
         Name,
         Occupation,
-        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) AS rn /* partition is not reducing the rows */
     FROM OCCUPATIONS;
+/* =========== lower and upper case */
+SELECT * FROM my_table WHERE UPPER(column_name) = UPPER('Search Term');
+SELECT * FROM my_table WHERE LOWER(column_name) = LOWER('Search Term');
+/* =========== COALESCE */
+COALESCE(value1, value2, value3, ...)
+SELECT COALESCE(NULL, 'Default Value');
+/* =========== avg */
+select floor(avg(population))
+from city;
+/* =========== regexp */
+select name
+from employees
+where name regexp '^[Kr]';
+/* =========== remove digit or character */
+select ceil(avg(salary) - avg(REGEXP_REPLACE(salary, '0', '')))
+from employees;
+/* =========== table alias and count (must aggregate with group by) */
+SELECT CONCAT(earnings, ' ', earnings_count)
+FROM (
+    SELECT earnings, COUNT(*) AS earnings_count
+    FROM (
+        SELECT salary * months AS earnings
+        FROM employee
+    ) AS tbl_earnings
+    GROUP BY earnings
+    ORDER BY earnings_count DESC
+    LIMIT 1
+) AS most_frequent_earnings;
